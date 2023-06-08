0d0ebd2ff7480642c744c927d0e04b86423d1543
Cyrbuzz <cyrbuzz@foxmail.com>
2023-06-08 20:05:27 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import App from "./App.vue";
 import { router } from "./router";
 import { createPinia } from "pinia";
 
-export const dsn = import.meta.env.SENTRY_DSN
+export const dsn = {
+  dsn: import.meta.env.SENTRY_DSN
+}
 
 const app = createApp(App);
 const pinia = createPinia();
